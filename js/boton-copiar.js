8f2d347cc11cdbbbf4a46f6b58c635ff8b079835
isaza-01 <jefferson.isaza01@unicatolica.edu.co>
2022-07-23 12:38:58 -0500

    corrigiendo errores de responsive

--- js/boton-copiar.js
+++ js/boton-copiar.js
@@ -3,24 +3,8 @@ var resetCopiar = document.querySelector ("#tarea");
 
 BotonCopiar.addEventListener ("click",function(event){
     event.preventDefault ();
-    
-
-    //creación del botón pegar
     var botonesInvisibles = document.querySelector (".botones");
     botonesInvisibles.classList.add ("invisible");
-    var botonPegar = document.createElement ("button");
-    botonPegar.setAttribute("id", "botonPegar");
-
-    var BotonDescripcion = document.createElement ("p");
-    botonPegar.classList.add("botones","encriptar","encriptar:hover");
-    var botonNuevo = document.querySelector ("#botonNuevo");
-    
-    botonNuevo.appendChild(botonPegar);
-    botonPegar.appendChild (BotonDescripcion);
-    var p = "Pegar";
-    BotonDescripcion.textContent = p;
-
-    //hasta aquí
 
     //capturar el valor de la caja textarea para poderla copiar y enviar a la otra caja de encriptación
     var inputCopiar = document.querySelector (".ingresar_texto_encriptador2");
@@ -29,19 +13,34 @@ BotonCopiar.addEventListener ("click",function(event){
     inputCopiar.textContent = copiandoTexto;
     inputCopiar.classList.add ("invisible");
 
+    
+    
+    //creación del botón pegar
+    var botonPegar = document.createElement ("button");
+    botonPegar.setAttribute("id", "botonPegar");
+    var contenidoBoton = document.createElement ("p");
+    botonPegar.classList.add("botones","encriptar","encriptar:hover");
+    botonesInvisibles.appendChild (botonPegar);
+    botonPegar.appendChild (contenidoBoton);
+    var p = "Pegar";
+    contenidoBoton.textContent = p;
+    //hasta aquí
+
     resetCopiar.innerHTML="";
 
-    var btonPegar = document.querySelector ("#botonPegar");
-    btonPegar.addEventListener ("click",function(event){
+    var botonPegar = document.querySelector ("#botonPegar");
+    botonPegar.addEventListener ("click",function(event){
+    
         event.preventDefault();
         inputCopiar.classList.remove("invisible");
-
         botonPegar.classList.add ("invisible");
         botonesInvisibles.classList.remove("invisible");
-
     });
 
 });
+
+
+
 
 
 
